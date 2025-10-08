5d6a2a1f336f3d68cc335f79bc4e1903f0f7077f
S-aba <Sabazelalem4@gmail.com>
2025-10-08 18:52:41 -0500

    update #2

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,6 +1,4 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
-import personalPhoto from "./photo.jpg"
+import personalPhoto from "./assets/photo.jpg";
 
 export function Welcome() {
   return (
@@ -9,21 +7,23 @@ export function Welcome() {
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
             <p
-              className='text-center'>Hi, I&apos;m [Saba]. Welcome to my portfolio!</p>
+              className='text-center'>Hi, I&apos;m <span className="text-pink-600 border pr-1 pl-1 border-pink-600 rounded-4xl animate-bounce ...">Saba</span>. Welcome to my portfolio!</p>
             <img
               src={personalPhoto}
               alt="photo of me"
-              className=" rounded-4xl border border-gray-200 dark:border-gray-700"
+              className=" rounded-4xl border border-pink-200 dark:border-pink-700"
             />
+
           </div>
         </header>
         <div>
-          <nav>3
+          <nav>
             <p>
               I am an aspiring software developer, here are my projects.
             </p>
             <p className="text-center"> This portfolio.
               <br /> stay tuned for more! </p>
+            <p className="text-center">sabazelolem@gmail.com</p>
           </nav>
         </div>
       </div>
