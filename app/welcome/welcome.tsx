86a9c0a427b6cb8b24cdd7d5e72e3f791b89701d
S-aba <Sabazelalem4@gmail.com>
2025-11-21 00:56:16 -0600

    another update - added a project

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,4 +1,5 @@
 import personalPhoto from "./assets/photo.jpg";
+import { Link } from "react-router-dom";
 
 export function Welcome() {
   return (
@@ -6,27 +7,33 @@ export function Welcome() {
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
-            <p
-              className='text-center'>Hi, I&apos;m <span className="text-pink-600 border pr-1 pl-1 border-pink-600 rounded-4xl animate-bounce ...">Saba</span>. Welcome to my portfolio!</p>
+            <p className="text-center">
+              Hi, I&apos;m{" "}
+              <span className="text-pink-600 border pr-1 pl-1 border-pink-600 rounded-4xl animate-bounce ...">
+                Saba
+              </span>
+              . Welcome to my portfolio!
+            </p>
             <img
               src={personalPhoto}
               alt="photo of me"
-              className=" rounded-4xl border border-pink-200 dark:border-pink-700"
+              className="rounded-4xl border border-pink-200 dark:border-pink-700"
             />
-
           </div>
         </header>
         <div>
           <nav>
-            <p>
-              I am an aspiring software developer, here are my projects.
+            <p>I am an aspiring software developer, here are my projects.</p>
+            <p className="text-center">
+              {" "}
+              This portfolio.
+              <Link to="https://triviallock.com/">Trivial.lock</Link>
+              <br /> stay tuned for more!{" "}
             </p>
-            <p className="text-center"> This portfolio.
-              <br /> stay tuned for more! </p>
             <p className="text-center">sabazelolem@gmail.com</p>
           </nav>
         </div>
       </div>
-    </main >
+    </main>
   );
-};
+}
